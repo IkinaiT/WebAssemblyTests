8278abf13c3400693987506509152a68736926a9
IkinaiT <ikinai@yandex.ru>
2024-05-06 18:20:08 +0300

    Добавьте файлы проекта.

---
+++ WebAssemblyTests/Settings/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAssemblyTests.Settings
+{
+    public class AppSettings
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; } = 0;
+    }
+}
