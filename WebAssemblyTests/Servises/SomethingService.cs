ec2dd36d5fe256b208a8bf797578f4e37078b74a
IkinaiT <ikinai@yandex.ru>
2024-05-06 18:27:23 +0300

    upd

---
+++ WebAssemblyTests/Servises/SomethingService.cs
@@ -0,0 +1,16 @@
+﻿using WebAssemblyTests.Settings;
+
+namespace WebAssemblyTests.Servises
+{
+    public class SomethingService
+    {
+        string name;
+        int age;
+
+        public SomethingService(AppSettings appSettings)
+        {
+            name = appSettings.Name;
+            age = appSettings.Age;
+        }
+    }
+}
